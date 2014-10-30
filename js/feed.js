6c574bf41f3ee2ac88878648ab26e1cd0e9a9bd3
Samuli Suomi <smsuom@utu.fi>
2014-10-30 23:41:51 +0200

    Create first sketch

--- js/feed.js
+++ js/feed.js
@@ -1,3 +1,10 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'laiva',
+        limit: 10,
+        clientId: '02133fc6740f436ea05348c5483877d0',
+        useHttp: true
+    });
+    feed.run();
 });
