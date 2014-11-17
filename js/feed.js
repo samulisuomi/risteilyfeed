bfd82ab24573d60876d6b5f037987851b999ebda
Samuli Suomi <smsuom@utu.fi>
2014-11-17 21:09:39 +0200

    More tests

--- js/feed.js
+++ js/feed.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	
 	var feed = new Instafeed({
         get: 'tagged',
         tagName: 'jouluristeily',
@@ -7,10 +8,127 @@ $(document).ready(function() {
         useHttp: true
     });
 	if(typeof(Storage) !== "undefined") {
-		document.getElementById("debug").innerHTML = "toimii";
-	} else {
-		document.getElementById("debug").innerHTML = "ei toimi";
-	}
+		// Ladataan storagesta:
+		if (get("cache") == 1) {
+			// Retrieve the object from storage
+			document.getElementById("debug").innerHTML = "Cached";
+		}
+		else {
+			document.getElementById("debug").innerHTML = "Non-cached";
+			feed.run();
+			// Put the object into storage
+/*
+			var images = []
+			var aHrefs = []
+			var imgSrcs = []
+			var aElements = document.links;
+			var imgElements = document.images;
+			var i;
+			for (i = 0; i < aElements.length; i++) {
+			    aHrefs[i] = aElements[i].href;
+			}
+			for (i = 0; i < imgElements.length; i++) {
+			    imgSrcs[i] = imgElements[i].src;
+			}
+			//localStorage.setItem('heroImg',hero);
+			var image = encode64(imgSrcs[0]);
+			document.getElementById("debug").innerHTML = document.images.length;
+			document.getElementById("testi").src = decode64(image);*/
 
-    feed.run();
+		}
+	} else {
+		document.getElementById("debug").innerHTML = "Selaimesi ei tue HTML5:n local storage -ominaisuutta.";
+	}
 });
+
+function get(name){
+   if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+      return decodeURIComponent(name[1]);
+}
+
+function encode64(input) {
+	var keyStr = "ABCDEFGHIJKLMNOP" +
+	       "QRSTUVWXYZabcdef" +
+	       "ghijklmnopqrstuv" +
+	       "wxyz0123456789+/" +
+	       "=";
+	 input = escape(input);
+	 var output = "";
+	 var chr1, chr2, chr3 = "";
+	 var enc1, enc2, enc3, enc4 = "";
+	 var i = 0;
+
+	 do {
+	    chr1 = input.charCodeAt(i++);
+	    chr2 = input.charCodeAt(i++);
+	    chr3 = input.charCodeAt(i++);
+
+	    enc1 = chr1 >> 2;
+	    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+	    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+	    enc4 = chr3 & 63;
+
+	    if (isNaN(chr2)) {
+	       enc3 = enc4 = 64;
+	    } else if (isNaN(chr3)) {
+	       enc4 = 64;
+	    }
+
+	    output = output +
+	       keyStr.charAt(enc1) +
+	       keyStr.charAt(enc2) +
+	       keyStr.charAt(enc3) +
+	       keyStr.charAt(enc4);
+	    chr1 = chr2 = chr3 = "";
+	    enc1 = enc2 = enc3 = enc4 = "";
+	 } while (i < input.length);
+
+	 return output;
+}
+
+function decode64(input) {
+	var keyStr = "ABCDEFGHIJKLMNOP" +
+	   "QRSTUVWXYZabcdef" +
+	   "ghijklmnopqrstuv" +
+	   "wxyz0123456789+/" +
+	   "=";
+	 var output = "";
+	 var chr1, chr2, chr3 = "";
+	 var enc1, enc2, enc3, enc4 = "";
+	 var i = 0;
+
+	 // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+	 var base64test = /[^A-Za-z0-9\+\/\=]/g;
+	 if (base64test.exec(input)) {
+	    alert("There were invalid base64 characters in the input text.\n" +
+	          "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+	          "Expect errors in decoding.");
+	 }
+	 input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+	 do {
+	    enc1 = keyStr.indexOf(input.charAt(i++));
+	    enc2 = keyStr.indexOf(input.charAt(i++));
+	    enc3 = keyStr.indexOf(input.charAt(i++));
+	    enc4 = keyStr.indexOf(input.charAt(i++));
+
+	    chr1 = (enc1 << 2) | (enc2 >> 4);
+	    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+	    chr3 = ((enc3 & 3) << 6) | enc4;
+
+	    output = output + String.fromCharCode(chr1);
+
+	    if (enc3 != 64) {
+	       output = output + String.fromCharCode(chr2);
+	    }
+	    if (enc4 != 64) {
+	       output = output + String.fromCharCode(chr3);
+	    }
+
+	    chr1 = chr2 = chr3 = "";
+	    enc1 = enc2 = enc3 = enc4 = "";
+
+	 } while (i < input.length);
+
+	 return unescape(output);
+}
