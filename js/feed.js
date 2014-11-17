6a861feaf38d3ea9ec5c7b13220f0d7b34fc3322
Samuli Suomi <smsuom@utu.fi>
2014-11-17 19:27:55 +0200

    Add html storage test

--- js/feed.js
+++ js/feed.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
-    var feed = new Instafeed({
+	var feed = new Instafeed({
         get: 'tagged',
         tagName: 'jouluristeily',
         limit: 10,
         clientId: '02133fc6740f436ea05348c5483877d0',
         useHttp: true
     });
+	if(typeof(Storage) !== "undefined") {
+		$("#debug").text("toimii!");
+	} else {
+		$("#debug").text("ei toimi!");
+	}
+
     feed.run();
 });
